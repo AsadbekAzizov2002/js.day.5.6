547052e6578bec122decd4eb2c8ba241d7c8c92d
AsadbekAzizov2002 <azizovasadbek489@gmail.com>
2023-08-15 01:41:12 +0500

    end

---
+++ main.js
@@ -0,0 +1,246 @@
+// let a = 321;
+// let num1 = a % 100;
+// let num2 = num1 % 10;
+// let num3 = num2 * 100;
+
+// console.log(num3+num2+numm1);
+  
+
+// let a = 321;
+// let num1 = a % 100;
+// let numm = num1 *10;
+// let num2 = a -21;
+// let num3 =num2 / 100;
+
+// console.log(numm+num3);
+
+// let a = 123;
+// let num1 = a % 100;
+// let num2 = num1 % 10;
+// let num3 = num2 *10;
+// let n = num3 + 100;
+
+// console.log(n);
+
+
+
+
+// let a = 1450;
+
+// result1=a/1000;
+// result2 = a % 1000;
+
+// console.log(result1);
+// console.log(result2);
+
+// let a = 45120;
+ 
+// result1=a%10000;
+// result2=result1/100;
+// result3=result1%1000;
+
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+
+// let n = 4500;
+ 
+// let N = n  / 60;
+
+
+// console.log(N,"minut");
+
+
+
+// let n = 360000,sekunt;
+// let N=n/3600;
+
+// console.log(N,"soat");
+
+
+
+
+
+
+// let n = 360000;
+// let N = n  / 60;
+// let m= n/36000;
+
+
+// console.log(n);
+// console.log(N,"minut");
+// console.log(m,"soat");
+
+
+
+// let l = 560;
+// let m= l/100;
+
+// console.log(m,metr);
+
+
+// let m = 560;
+// let n = m/1000;
+
+// console.log(n,"tonna");
+
+
+// let b =48794;
+// let kb = b / 1024;
+
+// console.log (kb,"kb");
+
+
+
+// let a =prompt("a son");
+// let b =prompt("b son");
+   
+// console.log(a / b ,"marta");
+
+
+// let a = prompt("ikki son kiriting")
+// if (a>0){
+    
+//     console.log(++a);
+// }else{
+//     console.log("manfiy son ekan");
+// }
+
+
+
+
+// let a = prompt("ikki son kiriting")
+// if (a>0){
+    
+//     console.log(++a);
+// }else{
+//     console.log(a-2);
+// }
+
+
+// let n = prompt("son kiriting");
+
+// if(n>0){
+//     console.log(++n);
+// }else if(n<0){
+//     console.log(--n);
+// }else if(n==0){
+//     console.log(10);
+// }
+
+
+// let a =Number(prompt("a son "));
+// let b =Number(prompt("b son "));
+// let c =Number(prompt("c son "));
+
+// if(a>0){
+//     console.log(a);
+// }else if(b>0){
+//     console.log(b);
+// }else if(c>0){
+//     console.log(c);
+// }
+
+
+// let a= Number(prompt("a son"));
+// let c= Number(prompt("c son"));
+
+// if(a>c){
+//     console.log(a);
+// }else if(a<c){
+//     console.log(c);
+// };
+
+
+// let a= Number(prompt("a son"));
+// let c= Number(prompt("c son"));
+
+// if(a<c){
+//     console.log(a);
+// }
+// else if(a>c){
+//     console.log(c);
+// }
+
+
+
+
+
+// let a= Number(prompt("a son"));
+// let b= Number(prompt("b son"));
+
+// if(a>b){
+//     console.log(a);
+// }else if(a<b){
+//     console.log(b);
+// }
+
+
+
+
+// let a= Number(prompt("a son"));
+// let b= Number(prompt("b son"));
+
+// if(a<b){
+//     console.log(b,"b son katta",a,"ason kichik");
+// }
+
+
+
+// let a= Number(prompt("a son"));
+// let b= Number(prompt("b son"));
+
+// if(a>b){
+//     result =a + b
+//     console.log(result);
+// }else if(a==b){
+//     console.log(0);
+//     console.log(a,b);
+// }
+
+
+// let a= Number(prompt("a son"));
+// let b= Number(prompt("b son"));
+// let c= Number(prompt("c son"));
+
+// if(a>b && a>c){
+//     console.log(a);
+// }else if(b>a && b>c){
+//     console.log(b);
+// }else if(c>a &&c>b){
+//     console.log(c);
+// };
+
+
+// a= Number(prompt("a son"));
+// let b= Number(prompt("b son"));
+// let c= Number(prompt("c son"));
+
+// if(a>b && a>c){
+//     console.log(b);
+
+// }else if(b>a && b>c){
+//     console.log(a);
+// }else if(c>a &&c>b){
+//     console.log(b);
+// };
+
+
+// a= Number(prompt("a son"));
+// let b= Number(prompt("b son"));
+// let c= Number(prompt("c son"));
+
+// if(a>b && a>c){
+//     console.log(b);
+// }else if(b>a && b>c){
+//     console.log(c);
+// }else if(c>a &&c>b){
+//     console.log(a);
+// } 
+// if(a>b && a>c){
+//     console.log(a);
+// }else if(b>a && b>c){
+//     console.log(b);
+// }else if(c>a &&c>b){
+//     console.log(c);}
